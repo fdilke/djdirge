7258b1956436fe6eb5f64f62fbfd4943c096e4db
Felix <knowledge-graph-dev@springernature.com>
2018-02-02 14:39:00 +0000

    refactored to separate explicit fragment files named by class

--- src/test/scala/com/fdilke/musicxml/ParseMusicTest.scala
+++ src/test/scala/com/fdilke/musicxml/ParseMusicTest.scala
@@ -1,19 +1,43 @@
 package com.fdilke.musicxml
 
 import org.scalatest.FreeSpec
+import org.scalatest.Matchers._
+
+import scala.reflect.ClassTag
+import scalaxb.XMLFormat
 
 class ParseMusicTest extends FreeSpec {
-  "ParseMusic" - {
-    "makes sense of a file" in {
-      try {
-        val parsed = ParseMusic("music/lg-22484616.xml")
 
-        println("parsed!! type " + parsed.getClass.getSimpleName)
-      } catch {
-        case ex: Exception =>
-          println("ex is a " + ex.getClass.getSimpleName)
-          ex.printStackTrace()
-      }
+  private def parses[T : XMLFormat : ClassTag](fragmentName: String) =
+    ParseMusic[T](s"fragments/$fragmentName.xml") shouldBe a[T]
+
+  "ParseMusic" - {
+    "makes sense of an Encoding" in {
+      parses[Encoding]("Encoding")
     }
+
+    "makes sense of an Identification" in {
+      parses[Identification]("Identification")
+    }
+
+    "makes sense of a Defaults" in {
+      parses[Defaults]("Defaults")
+    }
+
+    "makes sense of a Credit" in {
+      parses[Credit]("Credit")
+    }
+
+//    "makes sense of a file" in {
+//      try {
+//        val parsed = ParseMusic("music/lg-22484616.xml")
+//
+//        println("parsed!! type " + parsed.getClass.getSimpleName)
+//      } catch {
+//        case ex: Exception =>
+//          println("ex is a " + ex.getClass.getSimpleName)
+//          ex.printStackTrace()
+//      }
+//    }
   }
 }
